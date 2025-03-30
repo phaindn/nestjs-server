e768793b3742ba2a6afaf44b0bd345bcedae97a7
Felixx <phaindn@gmail.com>
2025-03-30 07:03:44 +0700

    add api, v1 module

---
+++ src/modules/api/api.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { V1ApiModule } from './v1/v1.module';
+import { ApiController } from './api.controller';
+
+@Module({
+  imports: [V1ApiModule],
+  controllers: [ApiController],
+})
+export class ApiModule {}
---
+++ src/modules/api/v1/v1.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { V1Controller } from './v1.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [AuthModule, UsersModule],
+  controllers: [V1Controller],
+})
+export class V1ApiModule {}
